3e3b64c2f0f4ee8f1fceaeb11a7df00c3c6f3c57
Uchenna-ibute <77361109+Uchenna-ibute@users.noreply.github.com>
2023-01-21 15:13:24 +0100

    Update store.js

--- modules/store.js
+++ modules/store.js
@@ -4,7 +4,7 @@ export default class store {
     if (localStorage.getItem('books') === null) {
       books = [];
     } else {
-      books = JSON.parse(localStorage.getItem('boks'));
+      books = JSON.parse(localStorage.getItem('books'));
     }
     return books;
   }
